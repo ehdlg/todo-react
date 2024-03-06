f8634329c145a4ad22d10da4fe04a18ef665e966
Enrique <121901959+ehdlg@users.noreply.github.com>
2024-03-06 14:48:17 +0100

    feat(styles): add css files for each component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
-import { useState, useEffect } from 'react';
+import TodoList from './components/TodoList';
+import Header from './components/Header';
 import './App.css';
+import AddTodo from './components/AddTodo';
 
-type TodoType = {
-  id: number;
-  title: string;
-  content: string;
-  completed: Date | null;
-  created: string;
-};
-type TodoResponse = {
-  todos: TodoType[];
-  length: number;
-};
 function App() {
-  const [todos, setTodos] = useState<TodoResponse>({ todos: [], length: 0 });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/todo')
-      .then((response) => response.json())
-      .then((data) => setTodos(data));
-  }, []);
-
-  console.log(todos.todos);
-
-  console.log(new Date(todos?.todos[0]?.created));
-
   return (
     <>
-      <h1>Welcome to the Todo List</h1>
-      <ul>
-        {todos.length > 0 &&
-          todos.todos.map((todo) => {
-            return <li>{todo.title}</li>;
-          })}
-      </ul>
+      <Header />
+      <AddTodo />
+      <TodoList />
     </>
   );
 }
