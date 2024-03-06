73ef59e04356fb9c9e40fd20d42ba7b78db1d944
Enrique <121901959+ehdlg@users.noreply.github.com>
2024-03-06 14:47:11 +0100

    feat(hook): add useGetTodos hook

--- src/hooks/useGetTodos.tsx
+++ src/hooks/useGetTodos.tsx
@@ -1,19 +1,44 @@
 import { useEffect, useState } from 'react';
-
-type TodoType = {
-  id: number;
-  title: string;
-  content: string;
-  completed: Date | null;
-  created: string;
-};
+import { type TodoType } from '../types';
 
 function useGetTodos() {
-  const [todos, setTodos] = useState<TodoType | null>(null);
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const [todosError, setTodoError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [shouldFetch, setShouldFetch] = useState(true);
+
+  const handleFetch = (newShouldFetch: boolean) => {
+    setShouldFetch(newShouldFetch);
+  };
 
   useEffect(() => {
+    if (!shouldFetch) return;
+
+    setLoading(true);
+
     fetch('http://localhost:3000/api/todo')
-      .then((response) => response.json())
-      .then((data) => setTodos(data));
-  }, [todos]);
+      .then(async (response) => {
+        if (response.status !== 200) {
+          const { error } = await response.json();
+          return setTodoError(error);
+        }
+
+        return response.json();
+      })
+      .then((data) => setTodos(data))
+      .catch(() => setTodoError('Could not connect to the database.'))
+      .finally(() => {
+        setLoading(false);
+        handleFetch(false);
+      });
+  }, [shouldFetch]);
+
+  return {
+    todos,
+    loading,
+    todosError,
+    handleFetch,
+  };
 }
+
+export default useGetTodos;
