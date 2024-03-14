97423f247768d1f1a0d1e23081a15be19648d6fd
= <=>
2024-03-14 09:12:58 +0100

    feat(filterTodo): add filterTodo functions

---
+++ src/components/lib/filterTodo.ts
@@ -0,0 +1,7 @@
+import { TodoType } from '../../types';
+
+export const filterTodos = {
+  all: (todos: TodoType[]) => todos,
+  active: (todos: TodoType[]) => todos.filter((todo) => todo.completed === 0),
+  completed: (todos: TodoType[]) => todos.filter((todo) => todo.completed === 1),
+};
