a265a629c6e943a538c4bc2185247b9e558758f8
= <=>
2024-03-14 09:10:55 +0100

    fix(Error): modify error messge

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,11 +1,7 @@
-import '../styles/Error.css';
-
 export function Error({ msg }: { msg: string }) {
   return (
     <div className='error-wrapper'>
-      <h3 className='error-title'>
-        There was an error trying to load the todos:
-      </h3>
+      <h3 className='error-title'>There was an error:</h3>
       <p className='error-message'>{msg}</p>
     </div>
   );
