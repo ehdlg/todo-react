42298ef4c561e020d52981c385eb637edd3d30cb
= <=>
2024-03-19 09:44:33 +0100

    feat(App): add Toaster to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import Main from './components/Main';
 import Header from './components/Header';
 import NewTodo from './components/NewTodo';
 import TodoProvider from './context/ContextProvider';
+import { Toaster } from 'sonner';
+import { toasterOptions } from './contants';
 import './styles/index.css';
 
 function App() {
   return (
     <TodoProvider>
+      <Toaster {...toasterOptions} />
       <Main>
         <Header />
         <NewTodo />
