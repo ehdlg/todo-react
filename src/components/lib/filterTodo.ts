3653d3fe69cba095816cb87ad697170e4224d7fa
= <=>
2024-03-19 09:50:35 +0100

    moved from components/lib to src/lib

--- src/components/lib/filterTodo.ts
+++
@@ -1,7 +0,0 @@
-import { TodoType } from '../../types';
-
-export const filterTodos = {
-  all: (todos: TodoType[]) => todos,
-  active: (todos: TodoType[]) => todos.filter((todo) => todo.completed === 0),
-  completed: (todos: TodoType[]) => todos.filter((todo) => todo.completed === 1),
-};
