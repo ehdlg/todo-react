fc7f1e3f4cccf67ec5ef4e138907dd02e4ba5322
= <=>
2024-03-19 09:48:50 +0100

    fix(Main): delete unused tailwind className

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 function Main({ children }: PropsWithChildren) {
-  return <main className='flex'>{children}</main>;
+  return <main>{children}</main>;
 }
 
 export default Main;
