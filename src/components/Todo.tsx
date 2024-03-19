e110bf6801f087f7a63cf84857228886914915b9
= <=>
2024-03-19 09:49:40 +0100

    feat(Todo): add sonner to Todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,11 +2,17 @@ import { TodoType } from '../types';
 import { useContext } from 'react';
 import '../styles/index.css';
 import TodoContext from '../context/context';
-import { completeTodo, deleteTodos } from './lib/db';
+import { completeTodo } from '../lib/db';
+import { toast } from 'sonner';
 
 function Todo({ todo }: { todo: TodoType }) {
   const { handleFetch } = useContext(TodoContext);
 
+  const handleChange = () => {
+    completeTodo(todo, handleFetch);
+
+    if (!todo.completed) toast.info('🎉Todo completed🎉');
+  };
   return (
     <div className={`todo ${todo.completed === 1 ? 'todo-completed' : ''}`} key={todo.id}>
       <input
@@ -14,12 +20,11 @@ function Todo({ todo }: { todo: TodoType }) {
         name='todo-complete'
         id={todo.id.toString()}
         defaultChecked={todo.completed === 1}
-        onChange={() => completeTodo(todo, handleFetch)}
+        onChange={handleChange}
       />
       <label htmlFor={todo.id.toString()} className='todo-title'>
         {todo.title}
       </label>
-      <button onClick={() => deleteTodos([todo.id], handleFetch)}>Delete todo</button>
     </div>
   );
 }
